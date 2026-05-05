698186d156b83c993c1e1f126d56ea24b08d0260
arw008-droid <arw008@ucsd.edu>
2026-05-04 19:45:14 -0700

    Did some of part 3 + added copy of expose html and renamed to index html

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 Name: Aron Wu
+
+## Pt 3 Check Your Understanding
+
+1. I wouldn't use a unit test for a messaging feature of a messaging app because the unit test would be testing many different components of the feature at the same time. Therefore, it would no longer be considered a unit test.
+2. I would use a unit test to test the max message limit for the same app because the feature is of a much smaller scope and the main debugging would be the logic of the code, not affecting other components. Therefore, it would fit the definition of a unit test better. 
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,171 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber Tests
+/**
+ * Test random US phone number
+ */
+test("(206) 342-8631 is a phone number", () => {
+  const res = isPhoneNumber('(206) 342-8631');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * Test random Canada phone number
+ */
+test("(035) 243-3401 is a phone number",() => {
+  const res = isPhoneNumber('(035) 243-3401');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * Test random Australian phone number
+ */
+test("+61 8 8711 6772 is a phone number",() => {
+  const res = isPhoneNumber('61 8 8711 6772');
+  expect(res).toBeFalsy();
+});
+
+/**
+ * Test random UK phone number
+ */
+test("+44 36 9339 4440 is a phone number", () => {
+  const res = isPhoneNumber('+44 36 9339 4440');
+  expect(res).toBeFalsy();
+});
+
+//isEmail Tests
+/** 
+ * 
+ */
+test("is an email", () => {
+  const res = isEmail('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is an email", () => {
+  const res = isEmail('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is an email", () => {
+  const res = isEmail('');
+  expect(res).toBeFalsy();
+});
+
+/**
+ * 
+ */
+test("is an email", () => {
+  const res = isEmail('');
+  expect(res).toBeFalsy();
+});
+
+// isStrongPassword Tests
+
+/**
+ * 
+ */
+test("is a strong password", () => {
+  const res = isStrongPassword('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is a strong password", () => {
+  const res = isStrongPassword('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is a strong password", () => {
+  const res = isStrongPassword('');
+  expect(res).toBeFalsy();
+});
+
+/**
+ * 
+ */
+test("is a strong password", () => {
+  const res = isStrongPassword('');
+  expect(res).toBeFalsy();
+});
+
+// isDate Tests
+
+/**
+ * 
+ */
+test("is a date", () => {
+  const res = isDate('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is a date", () => {
+  const res = isDate('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is a date", () => {
+  const res = isDate('');
+  expect(res).toBeFalsy();
+});
+
+/**
+ * 
+ */
+test("is a date", () => {
+  const res = isDate('');
+  expect(res).toBeFalsy();
+});
+
+// isHexColor Tests
+
+/**
+ * 
+ */
+test("is hexcolor", () => {
+  const res = isHexColor('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is hexcolor", () => {
+  const res = isHexColor('');
+  expect(res).toBeTruthy();
+});
+
+/**
+ * 
+ */
+test("is hexcolor", () => {
+  const res = isHexColor('');
+  expect(res).toBeFalsy();
+});
+
+/**
+ * 
+ */
+test("is hexcolor", () => {
+  const res = isHexColor('');
+  expect(res).toBeFalsy();
+});
